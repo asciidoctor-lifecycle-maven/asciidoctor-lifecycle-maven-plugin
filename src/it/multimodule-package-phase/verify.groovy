b4ecb57757ed37b02f6056b5f190ce5a6d2a7d8a
Ramón Rial <rrialq@gmail.com>
2019-08-14 13:00:08 +0200

    Refactorization finished

--- src/it/multimodule-package-phase/verify.groovy
+++ src/it/multimodule-package-phase/verify.groovy
@@ -1,4 +1,4 @@
-File pseudoThemeDir = new File( basedir, "asciidoctor-module/target/mydoctor-themes/wagon-provider-api" )
+File pseudoThemeDir = new File( basedir, "asciidoctor-module/target/asciidoctor-themes/wagon-provider-api" )
 File dependencyFile = new File( basedir, "asciidoctor-module/target/dependency/junit-4.11.jar" )
 
 assert dependencyFile.isFile() && ( ! pseudoThemeDir.exists() )
--- src/main/java/com/coutemeier/maven/plugins/asciidoctor/lifecycle/util/ArtifactUtil.java
+++ src/main/java/com/coutemeier/maven/plugins/asciidoctor/lifecycle/util/ArtifactUtil.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.coutemeier.maven.plugins.mydoctor.util;
+package com.coutemeier.maven.plugins.asciidoctor.lifecycle.util;
 
 import java.io.IOException;
 import java.util.List;
