42786aabf98df4c2724a13827902b739e61c667c
Ramón Rial <rrialq@gmail.com>
2019-08-25 19:16:48 +0200

    Issue #57 Fix unused private fields * Avoid unused private fields such as 'EXPECTED_GENERATED_INDEX_PATH'

--- src/test/java/com/coutemeier/maven/plugins/asciidoctor/lifecycle/AbstractAsciidoctorLifecycleMojoIT.java
+++ src/test/java/com/coutemeier/maven/plugins/asciidoctor/lifecycle/AbstractAsciidoctorLifecycleMojoIT.java
@@ -20,7 +20,6 @@ public class AbstractAsciidoctorLifecycleMojoIT {
     private static final String THEMESBASEDIR_PATH = "target/asciidoctor-themes";
     private static final String THEME_FILE_PATH = "/wagon-provider-api/META-INF/DEPENDENCIES";
     private static final String EXPECTED_THEME_FILE_PATH = THEMESBASEDIR_PATH + THEME_FILE_PATH;
-    private static final String EXPECTED_GENERATED_INDEX_PATH = "target/generated-docs/index.html";
 
     @Rule
     public final TestResources resources = new TestResources();
@@ -46,6 +45,5 @@ public class AbstractAsciidoctorLifecycleMojoIT {
         // We cannot test EXPECTED_GENERATED_INDEX_PATH
         // because asciidoctor-maven-plugin executes, because its skip parameter is different
         assertFilesNotPresent( basedir, EXPECTED_THEME_FILE_PATH );
-
     }
 }
